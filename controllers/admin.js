8e08827b87ebaac0ab816665ad7759d2c84f2954
Quy Nguyen <quynguyenbk98@gmail.com>
2020-03-16 00:11:06 +0700

    done 159

--- controllers/admin.js
+++ controllers/admin.js
@@ -19,7 +19,7 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     description: description
   })
-  .then(result => console.log(result))
+  .then(result => res.redirect('/admin/products'))
   .catch(err => console.log(err));
 };
 
@@ -29,16 +29,16 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId)
-  .then(([product]) => {
-    if (!product[0]) {
+  Product.findByPk(prodId)
+  .then((product) => {
+    if (!product) {
       return res.redirect('/');
     }
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
-      product: product[0]
+      product: product
     });
   })
   .catch(err => {
@@ -53,29 +53,44 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  // can use Product.update
+  Product.findByPk(prodId)
+  .then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.imageUrl = updatedImageUrl;
+    product.description = updatedDesc;
+    return product.save();
+  })
+  .then(result => res.redirect('/admin/products'))
+  .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.findAll()
+  .then(products => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
-  });
+  })
+  .catch(err => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  // Product.destroy({
+  //   where: {
+  //     id: prodId
+  //   }
+  // })
+  // .then(result => res.redirect('/admin/products'))
+  // .catch(err => console.log(err));
+  Product.findByPk(prodId)
+  .then(product => {
+    return product.destroy();
+  })
+  .then(result => res.redirect('/admin/products'))
+  .catch(err => console.log(err));
 };
